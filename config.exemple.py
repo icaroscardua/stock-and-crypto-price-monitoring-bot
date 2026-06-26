9ce1c474972bf268afef0e1429e53d97a246912c
Ícaro Scárdua <icaroscardua@gmail.com>
2026-06-26 13:51:42 -0300

    Initial setup: add .gitignore, requirements.txt and config.exemple.py

---
+++ config.exemple.py
@@ -0,0 +1,7 @@
+TELEGRAM_TOKEN = "your-telegram-bot-token"
+CHAT_ID = "your-chat-id"
+
+ASSETS = [
+    {"symbol" : "AAPL", "min" : 150.00, "max" : 200.00},
+    {"symbol" : "BTC-USD", "min" : 60000, "max" : 80000}, 
+]
