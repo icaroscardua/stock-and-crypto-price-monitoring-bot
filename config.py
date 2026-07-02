944cdf98f1209c5221d246052f3443fa7356be68
Ícaro Scárdua <icaroscardua@gmail.com>
2026-07-01 21:02:42 -0300

    Day 4: add monitoring loop in main.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ TELEGRAM_TOKEN = "8802586449:AAGM_m8JTYZ6XAcyIfMiMrmzCQccPNSZ60o"
 CHAT_ID = "906815010"
 
 ASSETS = [
-    {"symbol" : "AAPL", "min" : 150.00, "max" : 200.00},
-    {"symbol" : "BTC-USD", "min" : 60000, "max" : 80000}, 
+    {"symbol" : "AAPL", "min" : 150.00, "max" : 350.00},
+    {"symbol" : "BTC-USD", "min" : 40000, "max" : 80000}, 
 ]
---
+++ main.py
@@ -0,0 +1,32 @@
+import time 
+from monitor import search_price
+from alertas import send_alert
+from config import ASSETS
+
+CHECK_INTERVAL = 300
+
+def monitor():
+    print("🤖 Bot started! Monitoring assets...")
+    while True:
+        for asset in ASSETS:
+            symbol = asset["symbol"]
+            min_price = asset["min"]
+            max_price = asset["max"]
+
+            price = search_price(symbol)
+
+            if price is None:
+                continue
+
+            print(f"{symbol}: $ {price:.2f}")
+
+            if price < min_price:
+                send_alert(symbol, price, min_price, "below")
+            elif price > max_price:
+                send_alert(symbol, price, max_price, "above")
+        
+        print(f"⏳ Waiting {CHECK_INTERVAL} seconds...\n")
+        time.sleep(CHECK_INTERVAL)
+
+if __name__ == "__main__":
+    monitor()
