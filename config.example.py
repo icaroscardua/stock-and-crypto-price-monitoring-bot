19d7ebabb6843e80e7ce2fa8d9b78272aeffd4f1
Ícaro Scárdua <icaroscardua@gmail.com>
2026-07-01 22:53:43 -0300

    Update config.example.py to use .env file

--- config.example.py
+++ config.example.py
@@ -1,7 +1,12 @@
-TELEGRAM_TOKEN = "your-telegram-bot-token"
-CHAT_ID = "your-chat-id"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = int(os.getenv("CHAT_ID"))
 
 ASSETS = [
-    {"symbol" : "AAPL", "min" : 150.00, "max" : 200.00},
-    {"symbol" : "BTC-USD", "min" : 60000, "max" : 80000}, 
+    {"symbol": "AAPL", "min": 150.00, "max": 350.00},
+    {"symbol": "BTC-USD", "min": 40000, "max": 80000},
 ]
