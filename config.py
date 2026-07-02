48b54f25137fa46623a4c5dd1730b45577ec7871
Ícaro Scárdua <icaroscardua@gmail.com>
2026-07-01 22:52:08 -0300

    Security: migrate credentials to .env file

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-TELEGRAM_TOKEN = "8802586449:AAGM_m8JTYZ6XAcyIfMiMrmzCQccPNSZ60o"
-CHAT_ID = "906815010"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = int(os.getenv("CHAT_ID"))
 
 ASSETS = [
-    {"symbol" : "AAPL", "min" : 150.00, "max" : 350.00},
-    {"symbol" : "BTC-USD", "min" : 40000, "max" : 80000}, 
+    {"symbol": "AAPL", "min": 150.00, "max": 350.00},
+    {"symbol": "BTC-USD", "min": 40000, "max": 80000},
 ]
